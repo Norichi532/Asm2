68a345101500c45341bfbb6f87e66ed95f748b7d
Tan Phat <impphatdn99@gmail.com>
2024-10-24 12:11:06 +0700

    3 commit

--- models/pg_connector.js
+++ models/pg_connector.js
@@ -2,10 +2,10 @@ const { Pool, Client } = require('pg')
 
 // Define the DB connection 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'poldn0906',
-    host: '127.0.0.1',
+    user: 'asm_atn_db_al6l_user',
+    password: 'dsMLNcwo0cMykXWlq8fjS48BYsEAsNjo',
+    host: 'dpg-cscse7bv2p9s73fmvscg-a.oregon-postgres.render.com',
     port: 5432,
-    database: 'postgres',
+    database: 'asm_atn_db_al6l',
 })
 module.exports = pool;
